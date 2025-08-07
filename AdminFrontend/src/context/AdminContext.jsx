ac15bba57496a12b90a3f6df6c43ce1d8b62d08e
Prakash Kumar <prakashyadav9576@gmail.com>
2025-08-08 02:12:35 +0530

    make protected route

---
+++ AdminFrontend/src/context/AdminContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+
+// Create context
+const AdminContext = createContext();
+
+// Provider component
+export function AdminProvider({ children }) {
+  const [isAdmin, setIsAdmin] = useState(false); 
+  const [adminData, setAdminData] = useState(null); 
+  
+
+  return (
+    <AdminContext.Provider value={{ isAdmin, adminData, loginAsAdmin, logoutAdmin }}>
+      {children}
+    </AdminContext.Provider>
+  );
+}
+
+// Custom hook for using context
+export function useAdmin() {
+  return useContext(AdminContext);
+}
