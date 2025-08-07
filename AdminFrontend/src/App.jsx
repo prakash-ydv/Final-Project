7970ceba6d713ae31b226da8affd3573ac4d94e2
Prakash Kumar <prakashyadav9576@gmail.com>
2025-08-08 01:27:58 +0530

    setup router

--- AdminFrontend/src/App.jsx
+++ AdminFrontend/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  {
+    path: "/hello",
+    element: <p>Hello</p>,
+  },
+]);
 
 function App() {
   return (
     <div>
-      hello
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
