f049b429e45c5d969d74790d3c690780df724e8f
Prakash Kumar <prakashyadav9576@gmail.com>
2025-08-11 17:09:56 +0530

    setup

---
+++ VenderFrontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
