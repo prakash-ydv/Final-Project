98f986bb0b8fea291f84a606158b999f5baf1a77
Golu <golusiddharth88@gmail.com>
2025-08-11 20:49:01 +0530

    add comp

--- VenderFrontend/src/pages/HomePage.jsx
+++ VenderFrontend/src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Card from "./Resolvecard";
+import Box from "../components/Box";
 
 function HomePage() {
   return (
     <>
       <NavBar />
       <h1>Hello</h1>
+      <Card/>
+     <Box/>
     </>
   );
 }
