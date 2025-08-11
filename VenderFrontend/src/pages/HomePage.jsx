ce44530c64e617590e03d79b0807e23393096978
Golu <golusiddharth88@gmail.com>
2025-08-11 18:36:21 +0530

    comp

--- VenderFrontend/src/pages/HomePage.jsx
+++ VenderFrontend/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Card from "./Resolvecard";
 
 function HomePage() {
   return (
     <>
       <NavBar />
-      <h1>Hello</h1>
+      <Card/>
     </>
   );
 }
