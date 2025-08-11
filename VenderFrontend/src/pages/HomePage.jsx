ddc3dd6bcd58f532022f0c804ed5190252900f45
ashishkumar <ashish@kumar.in>
2025-08-11 18:34:39 +0530

    components

--- VenderFrontend/src/pages/HomePage.jsx
+++ VenderFrontend/src/pages/HomePage.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Box from "../components/Box";
 
 function HomePage() {
   return (
     <>
       <NavBar />
-      <h1>Hello</h1>
+      <div>
+        <div className=" flex flex-row">
+          <div className="border-1 w-90 h-37 ml-2 rounded-md"> 
+            <div className="justify-center flex mt-15 text-2xl ">Pending</div>
+          </div>
+          <div className="border-1 w-90 h-37 rouded-md">
+              <div className="justify-center flex mt-15 text-2xl">Total Target : 3</div>
+          </div>
+        </div>
+      </div>
+
+      <Box/>
+
     </>
   );
 }
