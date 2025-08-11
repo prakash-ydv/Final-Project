a90f7d905023563fe914230d5e87a19c099be1ba
Prakash Kumar <prakashyadav9576@gmail.com>
2025-08-11 17:10:01 +0530

    nav

---
+++ VenderFrontend/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+function HomePage() {
+  return (
+    <>
+      <NavBar />
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default HomePage;
